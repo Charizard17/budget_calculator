71ea8edbaa71b8a224725a15da519b813a001105
Charizard17 <3723esad@gmail.com>
2020-05-01 13:08:11 +0200

    item component modified + styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import './App.css';
-import ExpenseList from './components/ExpenseList';
-import ExpenseForm from './components/ExpenseForm';
-import Alert from './components/Alert';
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import uuid from "react-uuid";
+
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "car payment", amount: 400 },
+  { id: uuid(), charge: "credit card bill", amount: 1300 },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+  console.log(expenses);
+
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 }
